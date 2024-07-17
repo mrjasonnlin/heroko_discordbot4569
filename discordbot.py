c4315ac636d3ea22c25b8958f081c81125b74239
林至丞 <mrjasonnlin@gmail.com>
2024-07-18 03:08:45 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,38 +1,31 @@
+# 導入 Discord.py
 import discord
-import googletrans
-import os
-from pprint import pprint
-# 輸入自己Bot的TOKEN碼
-TOKEN = os.environ['TOKEN']
-SRCLanguage=os.environ['SRC']
-DSTLanguage=os.environ['DST']
 
-client = discord.Client()
+# client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-# 起動時呼叫
+
+# 調用 event 函式庫
 @client.event
+# 當機器人完成啟動時
 async def on_ready():
-    print('成功登入')
+    print('目前登入身份：', client.user)
 
-# 收到訊息時呼叫
+
 @client.event
+# 當有訊息時
 async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
+    # 排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
         return
-    
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+    # 如果包含 ping，機器人回傳 pong
+    if message.content == '安安':
+        await message.channel.send('安安~你好~')
 
-# Bot起動
-client.run(TOKEN)
+
+client.run(
+    'MTI2MjQ2OTc4MTIzMjI5MjA4Mg.GVui92.3sam-Zi8XW51pdsPZ1dNTnVJpLjtM2LVerXuFY')
+# TOKEN 在剛剛 Discord Developer
+# 那邊「BOT」頁面裡面
